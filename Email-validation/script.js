ae7b37f3a2de3934f796afa2e82458d68c601275
neven.shalata <nevenh.1994@gmail.com>
2022-04-06 16:12:06 +0300

    Email validation , Neven branch

---
+++ Email-validation/script.js
@@ -0,0 +1,24 @@
+
+
+function EmailValidation (Email)
+{
+    const template= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+if(template.test(Email))
+{
+    console.log(" valid Email " +" "+ true);
+}
+else
+{
+    console.log("Invalid Email" + " "+false);
+}
+
+}
+
+let x=prompt("please enter an Email");
+EmailValidation(x);
+
+
+
+ 
+
+
